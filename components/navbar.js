f9a5466df7292978639d43d8a469ee0f9f484e4d
Tuhin Saikh <tsaikh7@gmail.com>
2022-05-02 08:22:33 +0530

    First commit

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return `<h3><a href="./index.html">Home</a></h3>
+    <div id="option">
+        <h3><a href="./jewlary.html">jewlary</a></h3>
+        <h3><a href="./electronics.html">Electronics</a></h3>
+        <h3><a href="./login.html">Login</a></h3>
+        <h3><a href="./signup.html">Signup</a></h3>
+    </div>
+    `;
+}
+export {navbar};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {navbar} from "../components/navbar.js"
+import "../style/navbar.css"
+let navbar_div = document.getElementById("navbar");
+navbar_div.innerHTML = navbar();
